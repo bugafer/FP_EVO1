dfd1325635309b35f6f466c1ff89702f5b907e47
bugafer <fernandoiram57@gmail.com>
2025-08-26 16:18:32 -0600

    Add files via upload

---
+++ Eva1_1_Hola_Mundo/src/eva1_1_hola_mundo/Eva1_1_Hola_Mundo.java
@@ -0,0 +1,13 @@
+
+package eva1_1_hola_mundo;
+
+
+public class Eva1_1_Hola_Mundo {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Fundamentos de Programación");
+    }
+    
+}
