232c3c7cf0129c2035332b26fc0e5a244469eb09
bugafer <fernandoiram57@gmail.com>
2025-08-28 16:54:53 -0600

    Add files via upload

---
+++ Eva1_5_Variables/src/eva1_5_variables/Eva1_5_Variables.java
@@ -0,0 +1,25 @@
+
+package eva1_5_variables;
+
+public class Eva1_5_Variables {
+
+   
+    public static void main(String[] args) {
+        
+        //Declarar las variables
+        //tipo de dato   nombre del identificador
+        String nombre;
+        int edad;
+        double salario;
+        //Inicializacion:
+        //identificador  = valor;
+        //=--> operdor de asignacion
+        nombre=("Iram Fernando Bugarin Castañeda");
+        edad=29;
+        salario=1.1;
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+    }
+    
+}
