70f20eda9f518b582a130e2aa4c8a737de12926b
bugafer <fernandoiram57@gmail.com>
2025-09-02 16:03:40 -0600

    Add files via upload

---
+++ Eva1_8_Temperatura/src/eva1_8_temperatura/Eva1_8_Temperatura.java
@@ -0,0 +1,15 @@
+
+package eva1_8_temperatura;
+
+public class Eva1_8_Temperatura {
+
+ 
+    public static void main(String[] args) {
+        double celsius, far;
+        far=40;
+        celsius=(5*(far-32))/9;
+        System.out.println("la conversion de 40 grados farenheit a centigrados es: ");
+        System.out.println(celsius+"°Celsius");
+    }
+    
+}
